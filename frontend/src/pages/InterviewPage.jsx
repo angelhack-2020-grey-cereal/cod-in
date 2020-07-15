910fa4507f278d14c21e8514e92911ca10f5c7b0
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-15 21:32:28 +0900

    [#5] feature: intervewPage to react-webcam

--- frontend/src/pages/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import Webcam from 'react-webcam';
 
 export default function InterviewPage() {
   return (
     <div>
-      Interview
+      <Webcam
+        audio={true}
+        height={300}
+        width={300}
+        videoConstraints={{ width: 300, height: 300 }}
+      /> 
     </div>
   )
 }
