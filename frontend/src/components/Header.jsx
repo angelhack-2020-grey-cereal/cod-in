b193e51705f34de950ed1bfbc99ed6e0e76ce856
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-15 21:50:44 +0900

    [#5] refactor: rough design of interview page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+import styled from '@emotion/styled'
 
-import '../layout.css'
+const HeaderDiv = styled.div`
+  background-color: white;
+  min-height: 100px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-style: solid;
+`
 
 export default function Header() {
-
   return (
-    <div className="App-header">
+    <HeaderDiv>
       <h1>
         <Link to="/">codIt</Link>
       </h1>
       <h1>
         {} 님 (Tier 3) 256 엔젤코인
       </h1>
-    </div>
+    </HeaderDiv>
   )
 }
--- frontend/src/pages/InterviewPage.jsx
+++ frontend/src/pages/InterviewPage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
-import Webcam from 'react-webcam';
+import styled from '@emotion/styled'
+
+import InterviewSidebarContainer from '../components/InterviewSidebarContainer';
+
+
+const Container = styled.div`
+  height: calc(100% - 200px);
+  width: calc(100% - 300px);
+  position: fixed;
+  z-index: 1;
+  bottom: 0;
+  right: 0;
+  background-color: gray;
+  overflow-x: hidden;
+`
+
 
 export default function InterviewPage() {
   return (
-    <div>
-      <Webcam
-        audio={true}
-        height={300}
-        width={300}
-        videoConstraints={{ width: 300, height: 300 }}
-      /> 
-    </div>
+    <Container>
+      <InterviewSidebarContainer />
+    </Container>
   )
 }
