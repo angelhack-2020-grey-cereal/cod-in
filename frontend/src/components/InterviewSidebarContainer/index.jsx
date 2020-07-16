720d2530156985a091e92a69548c2fd5b97f2bbb
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-16 13:25:15 +0900

    [resolve #9] webcam record and play on Sidebar

--- frontend/src/components/InterviewSidebarContainer/index.jsx
+++ frontend/src/components/InterviewSidebarContainer/index.jsx
@@ -1,9 +1,51 @@
 
-import React from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import Webcam from 'react-webcam';
+import ReactPlayer from 'react-player'
 import './stylesheet.scss';
 
+
 export default function InterviewSidebarContainer () {
+
+  const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const [link, setLink] = useState('');
+
+  const handleDataAvailable = useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm"
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+
+  const handleStopCaptureClick = useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+
+    const blob = new Blob(recordedChunks, {
+      type: "video/webm"
+    });
+    const url = URL.createObjectURL(blob);
+    setLink(url);
+  }, [mediaRecorderRef, setCapturing, recordedChunks]);
+
+
   return (
     <div className="InterviewSidebarContainer">
       <h1>문제 1</h1>
@@ -11,23 +53,40 @@ export default function InterviewSidebarContainer () {
       <div className="Webcam">
         <Webcam
           audio={true}
+          ref={webcamRef}
           height={200}
           width={250}
           videoConstraints={{ height: 200, width: 250 }}
         />
-        <Webcam
-          audio={true}
-          height={200}
-          width={250}
-          videoConstraints={{ height: 200, width: 250 }}
-        />
+        {recordedChunks.length > 0 ? (
+          <ReactPlayer 
+            url={link} 
+            controls={true}
+            height={200}
+            width={250}
+          />
+          ) : ( 
+            <div style={{height: 200, width: 250}}></div>
+          )
+        }
       </div>
-      <button 
-        className="CloseButton"
-        type='button'
-      >
-        테스트 종료하기
-      </button>
+      {capturing ? (
+        <button
+          className="Button"
+          type="button"
+          onClick={handleStopCaptureClick}
+        >
+          Test Stop
+        </button>
+      ) : (
+        <button 
+          className="Button"
+          type="buton"
+          onClick={handleStartCaptureClick}
+        >
+          Test Start
+        </button>
+      )}
     </div>
   )
 }
