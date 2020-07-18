6261f29d854f4f5a1510632c5cc133382e9b83de
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-18 10:23:09 +0900

    enhance: Main UI Layout with contants - WIP

--- frontend/src/components/MainBox/MoreButton.jsx
+++ frontend/src/components/MainBox/MoreButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons/faChevronDown'
 import './stylesheet.scss';
 
 export default function MoreButton() {
