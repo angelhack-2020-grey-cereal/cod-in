0f474024fbcdc23ed0274ef8eec24d45790486c8
Jinseo Park <jinseo@google.com>
2020-07-18 12:17:35 -0400

    minor

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -4,33 +4,29 @@ import './stylesheet.scss';
 import { UserContext } from '../../contexts';
 import { faCoins } from '@fortawesome/free-solid-svg-icons/faCoins';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons/faCaretDown';
 
 export default function Header() {
   const { user: me } = useContext(UserContext);
   return (
     <div className="Header">
       <div className="container">
-        <Link to="/">
+        <Link to="/" className="logo-wrapper">
           <img className="logo" src={require('../../images/logo-white.png')} alt="logo"/>
         </Link>
-        <div className="divider">
-          <div>About us</div>
-          <div className="divide">|</div>
-          <div>Shop</div>
-          <div className="divide">|</div>
-          <div>Leader Board</div>
-          <div className="divide">|</div>
-          <div className="user">
-            <div className="avatar" style={{ backgroundImage: `url(${me.avatar_url})` }}/>
-            <div className="info">
-              <div className="label">
-                <div className="name">{me.name} </div>
-                <div className="tier">(Tier {me.tier})</div>
-              </div>
-              <div className="balance"><FontAwesomeIcon className="icon" icon={faCoins}/>{me.coin} Angel</div>
+        <a href="#" className="link">About us</a>
+        <Link to="/shop" className="link">Shop</Link>
+        <a href="#" className="link">Leader Board</a>
+        <a href="#" className="user">
+          <div className="avatar" style={{ backgroundImage: `url(${me.avatar_url})` }}/>
+          <div className="info">
+            <div className="label">
+              {me.name} (Tier {me.tier})
+              <FontAwesomeIcon className="icon" icon={faCaretDown} size="sm"/>
             </div>
+            <div className="balance"><FontAwesomeIcon className="icon" icon={faCoins}/>{me.coin} Angel</div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
   );
