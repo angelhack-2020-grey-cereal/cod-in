3e83b4da251c62b01c8fefa5a7cee739c9e5da93
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-19 10:40:19 +0900

    implement: LeaderBoardPage header - complete

--- frontend/src/pages/LeaderBoardPage/index.jsx
+++ frontend/src/pages/LeaderBoardPage/index.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import './stylesheet.scss'
 export default function LeaderBoardPage () {
+  const tiers = ["Tier 1", "Tier 2", "Tier 3", "Tier 4", "Tier 5"];
+
+  const [tier, setTier] = useState(2);
+
   return (
     <div className="LeaderBoard">
-      
-
+      <div className="container">
+        <div className="header">
+          {tiers.map((value, index) => {
+            return (
+              <button 
+                className={index === tier ? "header-component-select" : "header-component"}
+                onClick={() => setTier(index)}
+              >
+                <div className="rank-font">Rank</div>
+                <div>{value}</div>
+              </button>
+            )
+          })}
+        </div>
+      </div>
     </div>
   )
 }
