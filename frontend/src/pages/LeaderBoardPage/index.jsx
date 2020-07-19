6f51f1b796659ff74f3dfaa83c1facd458900b53
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-07-19 12:43:18 +0900

    implement: LeaderBoardPage table - wip

--- frontend/src/pages/LeaderBoardPage/index.jsx
+++ frontend/src/pages/LeaderBoardPage/index.jsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { UserContext } from '../../contexts';
 import './stylesheet.scss'
 export default function LeaderBoardPage () {
+  const { user: me, setUser } = useContext(UserContext);
+
   const tiers = ["Tier 1", "Tier 2", "Tier 3", "Tier 4", "Tier 5"];
 
   const [tier, setTier] = useState(2);
@@ -20,6 +23,28 @@ export default function LeaderBoardPage () {
               </button>
             )
           })}
+        </div>
+        <div className="content">
+          <table className="table">
+            <tr className="table-header">
+              <th className="table-width">순위</th>
+              <th className="table-width">프로필</th>
+              <th className="table-width-2x"></th>
+              <th>티어</th>
+              <th>획득한 Angel</th>
+              <th>참여횟수</th>
+            </tr>
+            <tr className="table-body">
+              <td>a</td>
+              <td>
+                <div className="avatar" style={{ backgroundImage: `url(${me.avatar_url})` }}/>
+              </td>
+              <td>a</td>
+              <td>a</td>
+              <td>a</td>
+              <td>a</td>
+            </tr>
+          </table>
         </div>
       </div>
     </div>
